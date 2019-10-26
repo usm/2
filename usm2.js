2516edd41e477c4a77a6e474cc48dd45414203b8
Jonas Almeida <jalmeida@mathbiol.org>
2019-10-26 13:04:22 +1300

    ini

---
+++ usm2.js
@@ -0,0 +1,97 @@
+console.log('usm2.js loaded')
+
+usm=function(seq='acggctagagctag',abc=usm.unique(seq)){
+    console.log('usm fun')
+    // raw sequence
+    this.seq=seq
+    // unique alphabet
+    // this.abc=abc
+    this.edges=usm.edges(abc)
+    usm.iterate(this)
+}
+
+usm.unique=seq=>[...new Set(seq)]
+
+usm.rep=(v=0,n=2)=>[...new Array(n)].map(_=>v) // replicate v n times
+
+usm.range=((n,i0=0,d=1)=>[...Array(n)].map((_,i)=>i*d+i0)) // array with n numbers at d intervals starting with i0 
+
+usm.int2bin=(v,n=Math.floor(Math.log2(v))+1)=>{ // integer to binary as an array of length n
+    let bb=usm.rep(0,n)
+    for(var i in bb){
+        let k = 2**(n-i-1)
+        if(v>=k){
+            bb[i]=1
+            v-=k
+        }
+    }
+    return bb
+}
+
+usm.edges=ab=>{ // calculate compact edges of an alphabet
+    const n = Math.ceil(Math.log2(ab.length)) // dimensions needed for compact notation
+    //const m = ab.length // alphabet length
+    const edges={}
+    ab.forEach((a,i)=>{
+        edges[a]=usm.int2bin(i,n)
+        if(edges[a].length==0){
+            edges[a]=[0]
+        }
+    })
+    return edges //usm.range(m).map(v=>usm.int2bin(v,n))
+}
+
+usm.iterate=(u)=>{
+    u.forward=[]
+    u.backward=[]
+    const n=u.seq.length
+    u.edges[Object.keys(u.edges)[0]].forEach((_,d)=>{ // for each dimension
+        u.forward[d]=usm.rep(0.5,n)
+        u.backward[d]=usm.rep(0.5,n)
+        let funForward = function(m=n){
+            u.forward[d][0]=u.backward[d][0]+(u.edges[u.seq[0]][d]-u.backward[d][0])/2
+            for(let i=1 ; i<m ; i++){
+                u.forward[d][i]=u.forward[d][i-1]+(u.edges[u.seq[i]][d]-u.forward[d][i-1])/2
+            }    
+        }
+        let funBackward = function (){
+            u.backward[d][n-1]=u.forward[d][n-1]+(u.edges[u.seq[n-1]][d]-u.forward[d][n-1])/2
+            for(let i=n-2 ; i>=0 ; i--){
+                u.backward[d][i]=u.backward[d][i+1]+(u.edges[u.seq[i]][d]-u.backward[d][i+1])/2
+            }    
+        }
+        funForward()
+        funBackward()
+        funForward( n>100 ? 100 : n )
+        for(let k=0 ; k < 100/n ; k++){
+            funBackward()
+            funForward()
+        }
+
+
+
+        
+        /*
+        // seed forward with backward tail
+        u.forward[d][0]=u.backward[d][0]+(u.edges[u.seq[0]][d]-u.backward[d][0])/2
+        for(let i=1 ; i<n ; i++){
+            u.forward[d][i]=u.forward[d][i-1]+(u.edges[u.seq[i]][d]-u.forward[d][i-1])/2
+        }
+        // backward seeded with forward tail
+        u.backward[d][n-1]=u.forward[d][n-1]+(u.edges[u.seq[n-1]][d]-u.forward[d][n-1])/2
+        for(let i=n-2 ; i>=0 ; i--){
+            u.backward[d][i]=u.backward[d][i+1]+(u.edges[u.seq[i]][d]-u.backward[d][i+1])/2
+        }
+        */
+        //debugger
+    })
+
+
+    //debugger
+    return u
+}
+
+
+
+
+usm()
