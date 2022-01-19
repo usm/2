e797479d56b631fb468c288ce6c10beac4b8bbbc
Jonas Almeida <jalmeida@mathbiol.org>
2022-01-19 08:12:31 -0500

    don't sort string alphabets when provided

--- usm2.js
+++ usm2.js
@@ -6,10 +6,10 @@ usm = function(seq='acggctagagctag',abc){
     }else{
         this.seq=seq
     }
+    abc=abc||usm.unique(this.seq).sort() // note sort if undefined
     if(typeof(abc)=="string"){
         abc=abc.split('')
     }
-    abc=abc||usm.unique(this.seq).sort() // note sort if undefined
     this.edges=usm.edges(abc)
     usm.iterate(this)
     this.canvas=function(size=200,direction="forward",that=this,){
